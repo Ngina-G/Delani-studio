ac554845f8607fdc4939e04561e857a31b6e218c
Ngina-G <Nginagitau11@outlook.com>
2022-03-05 18:03:29 +0300

    adds the birth day calculator function

--- src/index.js
+++ src/index.js
@@ -16,3 +16,12 @@ let maleAkanName={
     Friday: "Afua",
     Saturday: "Ama"
 }
+
+function getDayOfWeek(date) {
+    let dayOfWeek = new Date(date).getDay();
+        return isNaN(dayOfWeek) ? null : 
+        ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'][dayOfWeek];
+    }
+getDayOfWeek("03-05-2022",)
+console.log(getDayOfWeek("03-05-2022", "male"));
+
