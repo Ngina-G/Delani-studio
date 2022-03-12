c68f29f9013bff10b42c09dc0e3b694e84b580b8
Ngina-G <Nginagitau11@outlook.com>
2022-03-13 00:15:32 +0300

    adds the hover effect but with

--- js/script.js
+++ js/script.js
@@ -8,4 +8,12 @@ $(".wwd-card").click(function() {
     $('.wwd-card').css('cursor', 'pointer')
   });
 
-$("")
+  //PORTFOLIO SECTION
+// $(".portfolio-images").hover("handlerIn, handlerOut" ,function(){
+//   $(this).css({"transform": "scale(1.2)"})
+// })
+$(".portfolio-images").hover(function() {
+  $(this).css("transform", "scale(1.2)");
+}, function() {
+  $(this).css({"transform": "scale(1)"});
+});
