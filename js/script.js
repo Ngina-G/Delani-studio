b9aae2a7c35150245e637e8b4334d897f2853f1f
Ngina-G <Nginagitau11@outlook.com>
2022-03-12 23:51:30 +0300

    adds jquery to the what we do section

--- js/script.js
+++ js/script.js
@@ -1,13 +1,11 @@
-$(".wwd-card").mouseenter(function() {
+$(".wwd-card").click(function() {
+    $(".wwd-p").toggle()
+    $(".image").fadeToggle(4000, "swing");
+    $("figcaption").css({"font-weight": "bold"})
+  })
+
+  $(".wwd-card").mouseenter(function() {
     $('.wwd-card').css('cursor', 'pointer')
   });
 
-$(".image").click(function() {
-    $(".wwd-p").toggle()
-
-  });
-
-
-  $(".image").click(function() {
-    $("figcaption").css({"font-weight": "bold"})
-  });
+$("")
