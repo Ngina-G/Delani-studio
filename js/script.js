acf3f5294ffaedd5d723a4749e4c809c892cf881
Ngina-G <Nginagitau11@outlook.com>
2022-03-13 14:58:25 +0300

    adds proper jquery paragraph to what we do section

--- js/script.js
+++ js/script.js
@@ -1,19 +1,29 @@
-$(".wwd-card").click(function() {
-    $(".wwd-p").toggle()
-    $(".image").fadeToggle(4000, "swing");
+$("#designP").click(function() {
+    $(".design-wwd-p").toggle()
+    $("#design-im").fadeToggle(400, "linear");
     $("figcaption").css({"font-weight": "bold"})
-  })
+})
+$("#developmentP").click(function() {
+  $(".development-wwd-p").toggle()
+  $("#development-im").fadeToggle(400, "linear");
+  $("figcaption").css({"font-weight": "bold"})
+})
+$("#productP").click(function() {
+  $(".product-wwd-p").toggle()
+  $("#product-im").fadeToggle(400, "linear");
+  $("figcaption").css({"font-weight": "bold"})
+})
 
   $(".wwd-card").mouseenter(function() {
     $('.wwd-card').css('cursor', 'pointer')
   });
 
   //PORTFOLIO SECTION
-// $(".portfolio-images").hover("handlerIn, handlerOut" ,function(){
-//   $(this).css({"transform": "scale(1.2)"})
-// })
+
 $(".portfolio-images").hover(function() {
-  $(this).css("transform", "scale(1.2)");
+  $(this).css("filter", "blur(5px)"); 
+  $(".div-port").show()
 }, function() {
-  $(this).css({"transform": "scale(1)"});
+  $(this).css("filter", "none");
+  $(".div-port").hide()
 });
