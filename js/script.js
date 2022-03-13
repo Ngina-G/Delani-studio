8213da6f7640760a8a34368e7bee33df55a016b7
Ngina-G <Nginagitau11@outlook.com>
2022-03-13 18:45:05 +0300

    adds functioning submit button and that does not reload the page, restricts a lack of entries and works on filled fields

--- js/script.js
+++ js/script.js
@@ -27,12 +27,34 @@ $(".container").hover(function() {
 });
 
   //MESSAGE ALERT
+// function myFunction() {
+//   let popup = document.getElementById("popup");
+//   if(document.getElementById("name").value.length == 0){
+//     popup.classList.toggle("show");
+//      document.getElementById("popup").innerHTML = 
+//         "Hi " + document.getElementById("name").value + ". We have received your message and we'll get back to you soon." ;}
+//   else if(document.getElementById("name").value === undefined){
+//         popup.classList.toggle("hide");
+//       }
+//   }
 function myFunction() {
   let popup = document.getElementById("popup");
-  if(document.getElementById("name").value == " "){
-    popup.classList.toggle("show");
-      document.getElementById("popup").innerHTML = 
-        "Thankyou, " + document.getElementById("name").value + ". We have received your message and we'll get back to you soon." ;
+  let empty = document.getElementById("name").value && document.getElementById("email").value
+    if(empty == ""){
+        popup.classList.toggle("none");
+        alert("Please fill in all the slots of the form!")
+        return false;
+  } else{
+          popup.classList.toggle("show");
+     document.getElementById("popup").innerHTML = 
+        "Hi " + document.getElementById("name").value + ". We have received your message and we'll get back to you soon." ;
   }
 }
- 
+$("input[type='submit']").click(function subFunction() { return false; });
+
+// function submitForm() {
+//   let frm = document.getElementsByName('contact-form')[0];
+//   frm.submit(); // Submit the form
+//   frm.reset();  // Reset all form data
+//   return false; // Prevent page refresh
+// }
